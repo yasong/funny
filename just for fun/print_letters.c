2f86fbb85cea66255e70cb7556a87b14bbfd020d
yasong <752198313@qq.com>
2017-09-07 22:17:00 +0800

    update

--- just for fun/print_letters.c
+++ just for fun/print_letters.c
@@ -2,7 +2,7 @@
 * @Author: Xiaokang Yin
 * @Date:   2017-09-07 18:14:20
 * @Last Modified by:   Xiaokang Yin
-* @Last Modified time: 2017-09-07 19:35:50
+* @Last Modified time: 2017-09-07 22:06:29
 */
 
 #include "print_letters.h"
@@ -14,18 +14,34 @@
 //TODO read sentence from file.
 void help()
 {
-    print "Example:\n";
-    print "./print_letters B \"hello world\"\n";
-    print "The first agrc is the program name.\n";
-    print "The second is the letter.\n";
-    print "The last one is the sentence.\n";
+    printf("Example:\n");
+    printf("./print_letters B \"hello world\"\n");
+    printf( "The first agrc is the program name.\n");
+    printf("The second is the letter.\n");
+    printf( "The last one is the sentence.\n");
 
 }
 
 
 void print_sentence_with_itself(char *ch);  //use the letter itself to print the letter 
 
-void print_sentence_with_letter(char letter, char *ch); //use the designated letter to print the letter 
+void print_sentence_with_letter(char letter, char *ch); //use the designated letter to print the letter
+void print_array(char *array, int len);
+void print_array(char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    char space = ' ';
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        for (j = 0; j < offset; ++j){
+            printf("%c",array[j + i* offset]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 
 /**
@@ -38,17 +54,18 @@ void print_sentence_with_itself(char *ch)
     char letter;
     char *array;
 
-    length = len(ch);
+    length = strlen(ch);
+    printf("length = %d\n", length);
     array = (char *)malloc((length * ROW +  2 * (length - 1) ) * LINE);
-    memset(array, " ", sizeof(char));// initialize the array with space
-
+    memset(array, ' ', sizeof(char));// initialize the array with space
     for (i = 0; i < length; ++i){
         if (ch[i] == 'A' || ch[i] == 'a'){
             letter = 'A';
-            print_A(letter, i, array);
+            print_A(letter, i, array, length);
 
         }
     }
+    print_array(array, length);
 }
 
 void print_sentence_with_letter(char letter, char *ch)
@@ -65,7 +82,9 @@ int main(int argc, char **argv)
     }
 
     if (argc == 3){
-        print_sentence_with_letter(argv[1], argv[2])
+        char ch = *argv[1];
+        printf("%c\n", ch);
+        print_sentence_with_letter(ch, argv[2]);
     }
 
     return 0;
--- just for fun/print_letters.h
+++ just for fun/print_letters.h
@@ -7,41 +7,57 @@
 #define LINE 7
 #define ROW 5
 
-void print_A(char ch, int n, char *array);;
-void print_B(char ch, int n, char *array);;
-void print_C(char ch, int n, char *array);;
-void print_D(char ch, int n, char *array);;
-void print_E(char ch, int n, char *array);;
-void print_F(char ch, int n, char *array);;
-void print_G(char ch, int n, char *array);;
-void print_H(char ch, int n, char *array);;
-void print_I(char ch, int n, char *array);;
-void print_J(char ch, int n, char *array);;
-void print_K(char ch, int n, char *array);;
-void print_L(char ch, int n, char *array);;
-void print_M(char ch, int n, char *array);;
-void print_N(char ch, int n, char *array);;
-void print_O(char ch, int n, char *array);;
-void print_P(char ch, int n, char *array);;
-void print_Q(char ch, int n, char *array);;
-void print_R(char ch, int n, char *array);;
-void print_S(char ch, int n, char *array);;
-void print_T(char ch, int n, char *array);;
-void print_U(char ch, int n, char *array);;
-void print_V(char ch, int n, char *array);;
-void print_W(char ch, int n, char *array);;
-void print_X(char ch, int n, char *array);;
-void print_Y(char ch, int n, char *array);;
-void print_Z(char ch, int n, char *array);;
+void print_A(char ch, int n, char *array, int len);
+void print_B(char ch, int n, char *array, int len);
+void print_C(char ch, int n, char *array, int len);
+void print_D(char ch, int n, char *array, int len);
+void print_E(char ch, int n, char *array, int len);
+void print_F(char ch, int n, char *array, int len);
+void print_G(char ch, int n, char *array, int len);
+void print_H(char ch, int n, char *array, int len);
+void print_I(char ch, int n, char *array, int len);
+void print_J(char ch, int n, char *array, int len);
+void print_K(char ch, int n, char *array, int len);
+void print_L(char ch, int n, char *array, int len);
+void print_M(char ch, int n, char *array, int len);
+void print_N(char ch, int n, char *array, int len);
+void print_O(char ch, int n, char *array, int len);
+void print_P(char ch, int n, char *array, int len);
+void print_Q(char ch, int n, char *array, int len);
+void print_R(char ch, int n, char *array, int len);
+void print_S(char ch, int n, char *array, int len);
+void print_T(char ch, int n, char *array, int len);
+void print_U(char ch, int n, char *array, int len);
+void print_V(char ch, int n, char *array, int len);
+void print_W(char ch, int n, char *array, int len);
+void print_X(char ch, int n, char *array, int len);
+void print_Y(char ch, int n, char *array, int len);
+void print_Z(char ch, int n, char *array, int len);
 
 
-void print_A(char ch, int n, char *array);
+void print_A(char ch, int n, char *array, int len)
 {
     int i, j;
-
-    for ( i = 0; i < ROW, ++i){
-        for ( j = 9; j < LINE, ++j){
-            
+    int offset = len * (ROW + 2) - 2;
+    for ( j = 0; j < LINE; ++j){
+        if ( j == 0 )   array[(ROW + 2) * n + 2] = ch;
+        if ( j == 1){
+            array[(ROW + 2) * n + offset + 1] = ch;
+            array[(ROW + 2) * n + offset + 3] = ch;
         }
+        if ( j == 3){
+            array[(ROW + 2) * n + j * offset + 0] = ch;
+            array[(ROW + 2) * n + j * offset + 1] = ch;
+            array[(ROW + 2) * n + j * offset + 2] = ch;
+            array[(ROW + 2) * n + j * offset + 3] = ch;
+            array[(ROW + 2) * n + j * offset + 4] = ch;
+        }
+        else {
+            array[(ROW + 2) * n + j * offset + 0] = ch;
+            array[(ROW + 2) * n + j * offset + 4] = ch;
+        } 
+            
     }
 }
+
+#endif
