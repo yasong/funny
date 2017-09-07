5107e2115bc044f8eced0aff7d636ce9a01aa2ed
yasong <752198313@qq.com>
2017-09-07 18:32:02 +0800

    add file

---
+++ just for fun/print_letters.h
@@ -0,0 +1,34 @@
+#ifndef __PRINT_LETTERS_H_
+#define __PRINT_LETTERS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void print_A(char ch);
+void print_B(char ch);
+void print_C(char ch);
+void print_D(char ch);
+void print_E(char ch);
+void print_F(char ch);
+void print_G(char ch);
+void print_H(char ch);
+void print_I(char ch);
+void print_J(char ch);
+void print_K(char ch);
+void print_L(char ch);
+void print_M(char ch);
+void print_N(char ch);
+void print_O(char ch);
+void print_P(char ch);
+void print_Q(char ch);
+void print_R(char ch);
+void print_S(char ch);
+void print_T(char ch);
+void print_U(char ch);
+void print_V(char ch);
+void print_W(char ch);
+void print_X(char ch);
+void print_Y(char ch);
+void print_Z(char ch);
