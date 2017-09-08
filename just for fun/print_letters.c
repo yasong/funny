35dba521215239e5a2b1a944944c33713462aa9e
yasong <752198313@qq.com>
2017-09-08 09:30:21 +0800

    update

--- just for fun/print_letters.c
+++ just for fun/print_letters.c
@@ -2,7 +2,7 @@
 * @Author: Xiaokang Yin
 * @Date:   2017-09-07 18:14:20
 * @Last Modified by:   Xiaokang Yin
-* @Last Modified time: 2017-09-07 22:06:29
+* @Last Modified time: 2017-09-08 09:17:30
 */
 
 #include "print_letters.h"
@@ -32,14 +32,19 @@ void print_array(char *array, int len)
     int i;
     int j;
     int offset;
-    char space = ' ';
+    char space = 0;
     offset = len * (ROW + 2) - 2;
     for (i = 0; i < LINE; ++i){
         for (j = 0; j < offset; ++j){
-            printf("%c",array[j + i* offset]);
+            if (array[j + i* offset] == 0)
+                printf(" ");
+            else printf("%c",array[j + i* offset]);
         }
         printf("\n");
     }
+    /*for ( i = 0; i < LINE * ROW; ++i){
+         printf("%cs",array[i]);
+    }*/
     printf("\n");
 }
 
@@ -55,37 +60,234 @@ void print_sentence_with_itself(char *ch)
     char *array;
 
     length = strlen(ch);
-    printf("length = %d\n", length);
+    //printf("length = %d\n", length);
     array = (char *)malloc((length * ROW +  2 * (length - 1) ) * LINE);
-    memset(array, ' ', sizeof(char));// initialize the array with space
+    memset(array, 0, sizeof(char) * (length * ROW +  2 * (length - 1) ) * LINE);// initialize the array with space
     for (i = 0; i < length; ++i){
         if (ch[i] == 'A' || ch[i] == 'a'){
             letter = 'A';
             print_A(letter, i, array, length);
-
+        }
+        if (ch[i] == 'B' || ch[i] == 'b'){
+            letter = 'B';
+            print_B(letter, i, array, length);
+        }
+        if (ch[i] == 'C' || ch[i] == 'c'){
+            letter = 'C';
+            print_C(letter, i, array, length);
+        }
+        if (ch[i] == 'D' || ch[i] == 'd'){
+            letter = 'D';
+            print_D(letter, i, array, length);
+        }
+        if (ch[i] == 'E' || ch[i] == 'e'){
+            letter = 'E';
+            print_E(letter, i, array, length);
+        }
+        if (ch[i] == 'F' || ch[i] == 'f'){
+            letter = 'F';
+            print_F(letter, i, array, length);
+        }
+        if (ch[i] == 'G' || ch[i] == 'g'){
+            letter = 'G';
+            print_G(letter, i, array, length);
+        }
+         if (ch[i] == 'H' || ch[i] == 'h'){
+            letter = 'H';
+            print_H(letter, i, array, length);
+        }
+         if (ch[i] == 'I' || ch[i] == 'i'){
+            letter = 'I';
+            print_I(letter, i, array, length);
+        }
+        if (ch[i] == 'J' || ch[i] == 'j'){
+            letter = 'J';
+            print_J(letter, i, array, length);
+        }
+        if (ch[i] == 'K' || ch[i] == 'k'){
+            letter = 'K';
+            print_K(letter, i, array, length);
+        }
+        if (ch[i] == 'L' || ch[i] == 'l'){
+            letter = 'L';
+            print_L(letter, i, array, length);
+        }
+        if (ch[i] == 'M' || ch[i] == 'm'){
+            letter = 'M';
+            print_M(letter, i, array, length);
+        }
+        if (ch[i] == 'N' || ch[i] == 'n'){
+            letter = 'N';
+            print_N(letter, i, array, length);
+        }
+        if (ch[i] == 'O' || ch[i] == 'o'){
+            letter = 'O';
+            print_O(letter, i, array, length);
+        }
+        if (ch[i] == 'P' || ch[i] == 'p'){
+            letter = 'P';
+            print_P(letter, i, array, length);
+        }
+        if (ch[i] == 'Q' || ch[i] == 'q'){
+            letter = 'Q';
+            print_Q(letter, i, array, length);
+        }
+        if (ch[i] == 'R' || ch[i] == 'r'){
+            letter = 'R';
+            print_R(letter, i, array, length);
+        }
+        if (ch[i] == 'S' || ch[i] == 's'){
+            letter = 'S';
+            print_S(letter, i, array, length);
+        }
+        if (ch[i] == 'T' || ch[i] == 't'){
+            letter = 'T';
+            print_T(letter, i, array, length);
+        }
+        if (ch[i] == 'U' || ch[i] == 'u'){
+            letter = 'U';
+            print_U(letter, i, array, length);
+        }
+        if (ch[i] == 'V' || ch[i] == 'V'){
+            letter = 'V';
+            print_V(letter, i, array, length);
+        }
+        if (ch[i] == 'W' || ch[i] == 'w'){
+            letter = 'W';
+            print_W(letter, i, array, length);
+        }
+        if (ch[i] == 'X' || ch[i] == 'x'){
+            letter = 'X';
+            print_X(letter, i, array, length);
+        }
+        if (ch[i] == 'Y' || ch[i] == 'y'){
+            letter = 'Y';
+            print_Y(letter, i, array, length);
+        }
+        if (ch[i] == 'Z' || ch[i] == 'z'){
+            letter = 'Z';
+            print_Z(letter, i, array, length);
         }
     }
     print_array(array, length);
+    free(array);
 }
 
 void print_sentence_with_letter(char letter, char *ch)
 {
+    int i;
+    int length;
+    char *array;
 
+    length = strlen(ch);
+    //printf("length = %d\n", length);
+    array = (char *)malloc((length * ROW +  2 * (length - 1) ) * LINE);
+    memset(array, 0, sizeof(char) * (length * ROW +  2 * (length - 1) ) * LINE);// initialize the array with space
+    for (i = 0; i < length; ++i){
+        if (ch[i] == 'A' || ch[i] == 'a'){
+            print_A(letter, i, array, length);
+        }
+        if (ch[i] == 'B' || ch[i] == 'b'){
+            print_B(letter, i, array, length);
+        }
+        if (ch[i] == 'C' || ch[i] == 'c'){
+            print_C(letter, i, array, length);
+        }
+        if (ch[i] == 'D' || ch[i] == 'd'){
+            print_D(letter, i, array, length);
+        }
+        if (ch[i] == 'E' || ch[i] == 'e'){
+            print_E(letter, i, array, length);
+        }
+        if (ch[i] == 'F' || ch[i] == 'f'){
+            print_F(letter, i, array, length);
+        }
+        if (ch[i] == 'G' || ch[i] == 'g'){
+            print_G(letter, i, array, length);
+        }
+         if (ch[i] == 'H' || ch[i] == 'h'){
+            print_H(letter, i, array, length);
+        }
+         if (ch[i] == 'I' || ch[i] == 'i'){
+            print_I(letter, i, array, length);
+        }
+        if (ch[i] == 'J' || ch[i] == 'j'){
+            print_J(letter, i, array, length);
+        }
+        if (ch[i] == 'K' || ch[i] == 'k'){
+            print_K(letter, i, array, length);
+        }
+        if (ch[i] == 'L' || ch[i] == 'l'){
+            print_L(letter, i, array, length);
+        }
+        if (ch[i] == 'M' || ch[i] == 'm'){
+            print_M(letter, i, array, length);
+        }
+        if (ch[i] == 'N' || ch[i] == 'n'){
+            print_N(letter, i, array, length);
+        }
+        if (ch[i] == 'O' || ch[i] == 'o'){
+            print_O(letter, i, array, length);
+        }
+        if (ch[i] == 'P' || ch[i] == 'p'){
+            print_P(letter, i, array, length);
+        }
+        if (ch[i] == 'Q' || ch[i] == 'q'){
+            print_Q(letter, i, array, length);
+        }
+        if (ch[i] == 'R' || ch[i] == 'r'){
+            print_R(letter, i, array, length);
+        }
+        if (ch[i] == 'S' || ch[i] == 's'){
+            print_S(letter, i, array, length);
+        }
+        if (ch[i] == 'T' || ch[i] == 't'){
+            print_T(letter, i, array, length);
+        }
+        if (ch[i] == 'U' || ch[i] == 'u'){
+            print_U(letter, i, array, length);
+        }
+        if (ch[i] == 'V' || ch[i] == 'V'){
+            print_V(letter, i, array, length);
+        }
+        if (ch[i] == 'W' || ch[i] == 'w'){
+            print_W(letter, i, array, length);
+        }
+        if (ch[i] == 'X' || ch[i] == 'x'){
+            print_X(letter, i, array, length);
+        }
+        if (ch[i] == 'Y' || ch[i] == 'y'){
+            print_Y(letter, i, array, length);
+        }
+        if (ch[i] == 'Z' || ch[i] == 'z'){
+            print_Z(letter, i, array, length);
+        }
+    }
+    print_array(array, length);
+    free(array);
 }
+
 int main(int argc, char **argv) 
 {
+     int i;
     if (argc == 1){
         help();
     }
     if (argc == 2){
         print_sentence_with_itself(argv[1]);
     }
-
-    if (argc == 3){
+    if (argc > 2){
+        for (i = 2; i < argc; ++i){
+            char ch = *argv[1];
+            //printf("%c\n", ch);
+            print_sentence_with_letter(ch, argv[i]);
+        }
+    }
+    /*if (argc == 3){
         char ch = *argv[1];
         printf("%c\n", ch);
         print_sentence_with_letter(ch, argv[2]);
-    }
+    }*/
 
     return 0;
 }
--- just for fun/print_letters.h
+++ just for fun/print_letters.h
@@ -40,19 +40,21 @@ void print_A(char ch, int n, char *array, int len)
     int i, j;
     int offset = len * (ROW + 2) - 2;
     for ( j = 0; j < LINE; ++j){
-        if ( j == 0 )   array[(ROW + 2) * n + 2] = ch;
-        if ( j == 1){
-            array[(ROW + 2) * n + offset + 1] = ch;
-            array[(ROW + 2) * n + offset + 3] = ch;
+        if ( j == 0 ){
+            array[(ROW + 2) * n + 2] = ch;
         }
-        if ( j == 3){
+        else if ( j == 1){
+            array[(ROW + 2) * n + j * offset + 1] = ch;
+            array[(ROW + 2) * n + j * offset + 3] = ch;
+       }
+        else if ( j == 3){
             array[(ROW + 2) * n + j * offset + 0] = ch;
             array[(ROW + 2) * n + j * offset + 1] = ch;
             array[(ROW + 2) * n + j * offset + 2] = ch;
             array[(ROW + 2) * n + j * offset + 3] = ch;
             array[(ROW + 2) * n + j * offset + 4] = ch;
         }
-        else {
+       else {
             array[(ROW + 2) * n + j * offset + 0] = ch;
             array[(ROW + 2) * n + j * offset + 4] = ch;
         } 
@@ -60,4 +62,553 @@ void print_A(char ch, int n, char *array, int len)
     }
 }
 
+void print_B(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == 3 || i == LINE - 1){
+            for ( j = 0; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    } 
+}
+void print_C(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            for ( j = 1; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    } 
+}
+void print_D(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            for ( j = 0; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    } 
+}
+
+void print_E(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == 3 || i == LINE - 1){
+            for ( j = 0; j < ROW; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    }
+}
+void print_F(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == 3){
+            for ( j = 0; j < ROW; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    }
+}
+
+void print_G(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            for ( j = 1; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else if ( i == 4){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else if ( i == 5){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    } 
+}
+
+void print_H(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 3){
+            for ( j = 0; j < ROW; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_I(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            for ( j = 0; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 2] = ch;
+        }
+    }
+}
+
+void print_J(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0){
+            for ( j = 0; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else if (i == LINE - 1){
+            for ( j = 0; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+        }
+        else if (i == LINE - 2){
+                array[n * 7 + i * offset ] = ch;
+                array[n * 7 + i * offset + 3] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 3] = ch;
+        }
+    }
+}
+
+void print_K(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    j = 4;
+    for (i = 0; i < LINE; ++i){
+        if (j > i ){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + j - i] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + i - j + 2] = ch;
+        }
+        
+    }
+}
+
+void print_L(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == LINE -1){
+            for ( j = 0; j < ROW; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    }
+}
+
+void print_M(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 1){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        if (i == 2 || i == 3){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 2] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_N(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 1){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        if (i == 3){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 2] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        if (i == 5){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_O(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0 || i == LINE - 1){
+            for ( j = 1; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+void print_P(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0 || i == 3 ){
+            for ( j = 0; j < ROW  - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }  
+        }
+        else if(i == 1 || i == 2){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+        }
+    }
+}
+
+void print_Q(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0 ){
+            for ( j = 1; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+        }
+        else if (i == LINE - 1){
+            for ( j = 1; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+        }
+        else if (i == LINE - 2){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+void print_R(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0  || i == 3){
+            for ( j = 0; j < ROW  - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }  
+        }
+        else if(i == 1 || i == 2){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else if(i == 4) {
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 2] = ch;
+        }
+        else if(i == 5) {
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+        }
+        else if(i == 6) {
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+void print_S(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0  || i == LINE - 1 || i == 3){
+            for ( j = 0; j < ROW  ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }  
+        }
+        else if(i == 1 || i == 2){
+            array[n * 7 + i * offset + 0] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+void print_T(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i ==0 ){
+            for ( j = 0; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }  
+        }
+        else{
+            array[n * 7 + i * offset + 2] = ch;
+        }
+    }
+}
+void print_U(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == LINE - 1){
+            for ( j = 1; j < ROW - 1; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+            
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_V(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == LINE - 1){
+            array[n * 7 + i * offset + 2] = ch;
+        }
+        else if (i == LINE - 2){
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_W(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 3 || i == 4){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 2] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else if (i == 5){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+    }
+}
+
+void print_X(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else if (i == 1 || i == LINE - 2){
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 2] = ch;
+        }
+    }
+}
+
+void print_Y(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0  || i == 1){
+            array[n * 7 + i * offset + 0] = ch;
+            array[n * 7 + i * offset + 4] = ch;
+        }
+        else if (i == 2){
+            array[n * 7 + i * offset + 1] = ch;
+            array[n * 7 + i * offset + 3] = ch;
+        }
+        else{
+            array[n * 7 + i * offset + 2] = ch;
+        }
+    }
+}
+
+void print_Z(char ch, int n, char *array, int len)
+{
+    int i;
+    int j;
+    int offset;
+    offset = len * (ROW + 2) - 2;
+    for (i = 0; i < LINE; ++i){
+        if (i == 0 || i == LINE - 1){
+            for ( j = 0; j < ROW ; ++j){
+                array[n * 7 + i * offset + j] = ch;
+            }
+        }
+        if (i == 1 ){
+            array[n * 7 + i * offset + 3] = ch;
+        }
+        if (i == 3 ){
+            array[n * 7 + i * offset + 2] = ch;
+        }
+        if (i == 5 ){
+            array[n * 7 + i * offset + 1] = ch;
+        }
+    }
+}
 #endif
